8059b90d5675796743287da0ee36b25a3ac21b6a
MD.Sabbir Rahman <mdsabbirrahmanm740@gmail.com>
2023-11-29 23:25:06 +0600

    button

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './About.css'
+const About = () => {
+    return (
+        <div>
+           {/* <div>
+           <img src="https://images.unsplash.com/photo-1520209759809-a9bcb6cb3241?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1nfGVufDB8fDB8fHww" alt="an old car"/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQBagjFSkJYUoWjuATGx4gFZ2xLDBe92aYwNCM_UWTZVGIvD1PhaLZzUVeBHeGc5NqmlE&usqp=CAU" style="--c: #668284;--b:1px;--s: 18px" alt="an old car"/>
+           </div> */}
+        </div>
+    );
+};
+
+export default About;
